b7c94d6d36f06b97019b560a174c71c564872bcb
sanderwl <sanderl@student.matnat.uio.no>
2017-11-01 19:11:58 +0100

    started code and moved files

---
+++ code/project4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main()
+{
+    double k = 1.3806505e-23; //J/K
+    double T = 1;
+    double beta = 1/k*T;
+
+
+
+
+
+    return 0;
+}
