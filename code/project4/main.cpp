8327661f471b8e552dc7d5db0c5185690d97763f
sanderwl <sanderl@student.matnat.uio.no>
2017-11-06 17:44:58 +0100

    littegranne

--- code/project4/main.cpp
+++ code/project4/main.cpp
@@ -35,11 +35,12 @@ mat randomMatrix(mat &A, int L){
 }
 
 
-double MP(int L, mat &A, vec &prob, int &acceptance, double &E, double &magnetic, double &E_2, double &Etemp, int cycle){
+void MP(int L, mat &A, vec &prob, int &acceptance, double &E, double &Mtemp, double &E_2, double &Etemp, int cycle, double &M, double &M_2){
     int xp, xn, yp, yn;
     double deltaE;
     for(int x = 0; x < L; x++){
         for(int y = 0; y < L; y++){
+
             xp = (x + 1) % L;
             yp = (y + 1) % L;
             xn = (x - 1 + L) % L;
@@ -51,7 +52,7 @@ double MP(int L, mat &A, vec &prob, int &acceptance, double &E, double &magnetic
                 A(x, y) *= -1;
                 Etemp += deltaE;
 
-                magnetic += 2*A(x,y);
+                Mtemp += 2*A(x,y); //Mean magnetic moment
                 acceptance += 1;
 
             }
@@ -59,10 +60,11 @@ double MP(int L, mat &A, vec &prob, int &acceptance, double &E, double &magnetic
     }
     E += Etemp;
     E_2 += Etemp*Etemp;
+    M += (Mtemp);
+    M_2 += Mtemp*Mtemp;
     toFile(cycle, Etemp);
 
     //cout << Etemp << endl;
-    return Etemp;
 }
 
 
@@ -77,11 +79,11 @@ int main()
 {
 
     double k = 1.0; //J/K
-    double T = 2.0; //kT/J
+    double T = 1.0; //kT/J
     double beta = 1.0/(k*T);
     double J = 1.0;
 
-    int mcs = 1000;
+    int mcs = 100000;
 
     int L = 2;
 
@@ -91,6 +93,7 @@ int main()
     //randomMatrix(A, L);
     //cout << A << endl;
     mat A = ones(L,L);
+
     double Etemp = 0;
 
     //Initial values of the temporary energy
@@ -106,24 +109,39 @@ int main()
 
     double E = 0;
     double E_2 = 0;
-    double magnetic = accu(A); //Magnetic moment
+    double Mtemp = 0;
+    double M = 0;
+    double M_2= 0;
 
     vec prob(17);
+    for(int i=-8; i <= 8; i+=4){
+        prob(i+8) = 0;
+    }
     for(int i=-8; i <= 8; i+=4){
         prob(i + 8) = exp(-i/T);
     }
     //cout << prob << endl;
     int acceptance = 0;
     for(int cycles=0;cycles<=mcs;cycles++){
-        MP(L, A, prob, acceptance, E, magnetic, E_2, Etemp, cycles);
+        MP(L, A, prob, acceptance, E, Mtemp, E_2, Etemp, cycles, M, M_2);
         toFile(cycles, Etemp);
 
     }
     double averegeE = E/(L*L*mcs); //Average energy
     double averegeESquared = E_2/(L*L*mcs);
+
+    //double magnetic = accu(A); //Magnetic moment
+    double averegeM = (M)/(mcs);
+    double averegeMSquared = (M_2/(mcs));
+
+    double PF = 2*exp(-8*J*beta) + 2*exp(8*J*beta) + 12; //Partition function
+    //double sus = beta*((M_2)/(PF) - (M*M)/(PF*PF));
+    double sus = beta*((averegeMSquared) - ((averegeM))*((averegeM)));//(L*L);
+
     cout << averegeE<< endl;
-    cout << averegeESquared << endl;
-    double PF = 2*exp(-8*J*beta) + 2*exp(8*J*beta) +12; //Partition fucntion
+    cout << averegeM << " " << averegeMSquared << endl;
+    cout << sus << endl;
+
 
     outFile.close();
     outFile2.close();
